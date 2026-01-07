4a0405fa8b64cba93cb689307d069210e22ecc59
jhajaya19 <jhajaya19@gmail.com>
2026-01-07 21:32:28 +0530

    Build the whole home page

--- Society360/frontend/src/home/Hero.js
+++ Society360/frontend/src/home/Hero.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
+import "../index"
 
 function Hero() {
-    return ( 
-        <div className='container mt-5' style={{backgroundColor: "#1B9AAA"}}>
-            <div className='row text-center text-white'>
-                <h1 style={{fontSize: "70px" , marginTop: "50px"}} className='mb-4'>Society360</h1>
-                <p style={{fontSize: "1.5rem"}}>The Smart Residential Management System designed to create a secure, integrated platform<br/> for modern living.</p>
-                <p></p>
-            </div>
-        </div>
-     );
+  return (
+    <div className="container-fluid mb-5" style={{ backgroundColor: "#1B9AAA" }}>
+      <div className="row text-center text-white">
+        <h1 style={{ fontSize: "70px", marginTop: "50px" }} className="mb-4">
+          Society360
+        </h1>
+        <p style={{ fontSize: "1.5rem" }}>
+          The Smart Residential Management System designed to create a secure,
+          integrated platform
+          <br /> for modern living.
+        </p>
+        <button className="p-2 btn btn-light fs-5 mt-3 mb-5" style={{width: "10%" , margin: "0 auto"}}>Learn more</button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- Society360/frontend/src/home/HomePage.js
+++ Society360/frontend/src/home/HomePage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import Ecosystem from './Ecosystem';
+import Experience from './Experience';
+import Footer from './Footer';
 
 function HomePage() {
     return ( 
         <>
           <Navbar/>
           <Hero/>
+          <Experience/>
+          <Ecosystem/>
+          <Footer/>
         </>
      );
 }
